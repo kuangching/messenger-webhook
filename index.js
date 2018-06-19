68997e8926e0b95e3ca2e03551bf3c30002e07a4
kuangching <qwer81812@gmail.com>
2018-06-19 18:56:20 +0800

    test

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.get("/",function (req, res){
     res.send("Deployed!");
 });
 
-app.get("/webhook", function (req, res){
-    if (req.query["hub.verify_token"] === "this_is_my_token"){
-        console.log("確認 web hook");
-        res.status(200).send(req.query["hub.challenge"]);
-    }else{
-        console.error("認證失敗，tokens不對");
-        res.sendStatus(403);
-    }
+// app.get("/webhook", function (req, res){
+//     if (req.query["hub.verify_token"] === "this_is_my_token"){
+//         console.log("確認 web hook");
+//         res.status(200).send(req.query["hub.challenge"]);
+//     }else{
+//         console.error("認證失敗，tokens不對");
+//         res.sendStatus(403);
+//     }
 
-});
+// });
