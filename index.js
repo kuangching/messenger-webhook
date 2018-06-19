7071a050474a54c438f302f02f2862686f5a064d
kuangching <qwer81812@gmail.com>
2018-06-19 23:46:41 +0800

    aitest

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ const
     body_parser = require('body-parser'),
     app = express().use(body_parser.json()); // creates express http server
 
+const PAGE_ACCESS_TOKEN = 'EAADveDdj9tABAMR7gSlgqLOH53gMJRWr9WEozElAMrRwzsHt1JQ8qZCMAqr0MO4BeR9sNeE9PAXjZCXN8s21cMabNHUfdZBpCqqECtPcKUQANcWcplL1ulvCICbWYZCnjIz6BZCEBZBIGmk16OaqBL6rtCTi7jdHKclfzT3ZChXkwZDZD';
+
+var apiai = require('apiai');
+var apiaiapp = apiai('83c8cbe12da743ff823cadbd60df09ae', '4d68a32def6841d7b26813cdca3bdc8f');
+
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
@@ -38,11 +43,21 @@ app.post('/webhook', (req, res) => {
         // Iterate over each entry - there may be multiple if batched
         body.entry.forEach(function(entry) {
 
-            // Get the webhook event. entry.messaging is an array, but
-            // will only ever contain one event, so we get index 0
+            // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
 
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         });
 
         // Return a '200 OK' response to all events
@@ -59,7 +74,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     /** UPDATE YOUR VERIFY TOKEN **/
-    const VERIFY_TOKEN = "this_is_my_token";
+    const VERIFY_TOKEN = PAGE_ACCESS_TOKEN;
 
     // Parse params from the webhook verification request
     let mode = req.query['hub.mode'];
@@ -82,3 +97,125 @@ app.get('/webhook', (req, res) => {
         }
     }
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+    let response;
+    var request = dialogdlow.textReqest(received_message.text, {sessionId: sender_psid});
+    request.on('response', function(response){
+        if(response.result.action == "train"){
+            strStation = response.result.parameters.start_station;
+            arrStation = response.result.parameters.arrive_station;
+            console.log('起點'+strStation);
+            console.log('終點'+arrStation);
+        }else {
+            // Check if the message contains text
+            if (received_message.text) {
+                switch (received_message.text) {
+                    case '公道價八萬一' :
+                        response = {
+                            "text": '你在大聲甚麼啦!'
+                        };
+                        break;
+                    case 'structure' :
+                        callSendStructuredAPI(sender_psid);
+                        break;
+                    case 'default' :
+                        response = {
+                            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+                        };
+                        break;
+                }
+            } else if (received_message.attachments) {
+                response = {
+                    "text": `這是附件`
+                };
+            }
+        }
+        // Sends the response message
+        if(received_message.text != 'structure'){
+            callSendAPI(sender_psid, response);
+        }
+    });
+    request.end();
+    
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+function callSendStructuredAPI(sender_psid){
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "generic",
+                    elements: [{
+                        title: '這是對的圖嗎?',
+                        subtitle: '按下按鈕吧',
+                        image_url: 'https://i.imgur.com/i9jFz7I.jpg',//'https://upload.wikimedia.org/wikipedia/ah/thumb/5/5d'
+                        buttons: [
+                            {
+                                type: "postback",
+                                title: "yes",
+                                payload: "yes"
+                            },
+                            {
+                                type: "postback",
+                                title: "no",
+                                payload: "no"
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    };
+
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
