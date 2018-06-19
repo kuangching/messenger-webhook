9680b7572de9d6abdecec0c3e9c4490cb74b3496
kuangching <30891693+kuangching@users.noreply.github.com>
2018-06-20 01:27:33 +0800

    28

--- index.js
+++ index.js
@@ -239,15 +239,15 @@ function TrainSchedule(str_station,arrStation,recipientId){
     console.log('查火車');
     var data = {
         FromCity: 9,
-        FromStation: 1239,
+        FromStation: 1215,
         FromStationName: 0,
         ToCity: 18,
-        ToStation: 5102,
+        ToStation: 1214,
         ToStationName: 0,
         TrainClass: 2,
         searchdate: moment().format('YYYY-MM-DD'),
         FromTimeSelect: moment().format('HHmm'),
-        ToTimeSelect: moment().add(1, 'hours').format('HHmm'),
+        ToTimeSelect: 2359,
         Timetype: 1
     };
 
